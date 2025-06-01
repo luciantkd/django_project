b7311db9166e0768a19c1d77542b0aa37a8ca5b4
luciantkd <lucianprocopciuc1@hotmail.com>
2025-06-01 20:47:00 +0100

    Updated views and settings; added static files and templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# how we want to handle routes
+posts = [
+    {
+        'author': 'Lucian',
+        'title' : 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '1st June, 2025'
+    },
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '2nd June, 2025'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': '3rd June, 2025'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
