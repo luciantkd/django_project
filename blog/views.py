dff477367fe38eaa8683287205936d5e53acdda3
Lucian Procopciuc <152325277+luciantkd@users.noreply.github.com>
2025-09-07 12:12:56 +0100

    database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Lucian',
-        'title' : 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': '1st June, 2025'
-    },
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': '2nd June, 2025'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': '3rd June, 2025'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
