01b87c271d189f0bfd1ab6247f1f72f5727a2cf1
Lucian Procopciuc <152325277+luciantkd@users.noreply.github.com>
2025-10-13 12:51:53 +0100

    create, update and delete posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView)
 from .models import Post
+from django.urls import reverse
 
 
 def home(request):
@@ -7,6 +15,50 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        # keep the author as the current user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    # only allow the author to edit
+    def test_func(self):
+        obj = self.get_object()
+        if self.request.user == obj.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    
+    # only allow the author to delete
+    def test_func(self):
+        obj = self.get_object()
+        if self.request.user == obj.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
